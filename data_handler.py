2c4a8728b026aeed9d74dfda5beb7150baa3c3e7
Bogi Kiss <bogikiss85@gmail.com>
2020-01-21 12:41:08 +0100

    add get statuses function

--- data_handler.py
+++ data_handler.py
@@ -5,13 +5,14 @@ import connection
 def get_card_status(cursor, status_id):
     """
     Find the first status matching the given id
+    :param cursor:
     :param status_id:
     :return: str
     """
     cursor.execute("""
     SELECT title from statuses
     where id = %(id)s;""",
-    {"id": status_id})
+                   {"id": status_id})
     statuses = cursor.fetchone()
     return statuses["title"]
 
@@ -34,6 +35,15 @@ def get_cards_for_board(cursor, board_id):
     cursor.execute("""
     SELECT * from cards
     WHERE board_id = %(board_id)s;""",
-    {"board_id": board_id })
+                   {"board_id": board_id})
     matching_cards = cursor.fetchall()
     return matching_cards
+
+
+@connection.connection_handler
+def get_statuses(cursor):
+    cursor.execute("""
+    SELECT * FROM statuses;""")
+
+    statuses = cursor.fetchall()
+    return statuses
